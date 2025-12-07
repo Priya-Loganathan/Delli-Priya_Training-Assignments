78294d6ccef8f4ddfcc4155eef913b7bfb0ff206
DELLI PRIYA LOGANATHAN <121166075+Priya-Loganathan@users.noreply.github.com>
2025-12-07 22:28:51 +0530

    Add files via upload

---
+++ ExceptionHandling_assignment/Regex.java
@@ -0,0 +1,17 @@
+package ExceptionHandling_assignment;
+
+public class Regex {
+
+	public static void main(String[] args) 
+	{
+		String regex = "^(001|\\+1)-\\d{3}-\\d{3}-\\d{4}$";
+
+        String p1 = "001-765-989-3421";
+        String p2 = "+1-456-765-9345";
+
+        System.out.println(p1 + " : " + p1.matches(regex));
+        System.out.println(p2 + " : " + p2.matches(regex));
+
+	}
+
+}
---
+++ ExceptionHandling_assignment/VoteCheck.java
@@ -0,0 +1,39 @@
+package ExceptionHandling_assignment;
+import java.util.Scanner;
+
+class InvalidAgeException extends Exception
+{
+    public InvalidAgeException(String message) 
+    {
+        super(message);
+    }
+}
+
+public class VoteCheck 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+
+        try 
+        {
+            checkAge(age);
+            System.out.println("Candidate is eligible to vote");
+        }
+        catch (InvalidAgeException e) 
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Method to check age
+    static void checkAge(int age) throws InvalidAgeException 
+    {
+        if (age < 18) 
+        {
+            throw new InvalidAgeException("Candidate is not eligible to vote");
+        }
+    }
+}

289f70db9e52d4bb2fde75cbdd515c2261cab2a1
DELLI PRIYA LOGANATHAN <121166075+Priya-Loganathan@users.noreply.github.com>
2025-12-11 10:12:02 +0530

    Add files via upload

---
+++ SeleniumAssignment_Day13/NavigateCommandsTest.java
@@ -0,0 +1,32 @@
+package SeleniumAssignment_Day13;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NavigateCommandsTest {
+  @Test
+  public void NavigateCommands() throws InterruptedException 
+  {
+	     WebDriver driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+		 
+		 driver.navigate().to("https://chatgpt.com/");
+		 Thread.sleep(2000);
+		 
+		 driver.navigate().to("https://github.com/");
+		 Thread.sleep(2000);
+
+		 driver.navigate().back();
+	     Thread.sleep(2000);
+	     
+	     driver.navigate().forward();
+	     Thread.sleep(2000);
+	     
+	     driver.navigate().refresh();
+	     Thread.sleep(2000);
+	     
+	     driver.quit();
+
+  }
+}
